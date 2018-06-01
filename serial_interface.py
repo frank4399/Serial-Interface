3f99761fdb0beaa27836b938d2cf9f2e3546e2d8
frank4399 <39400250+frank4399@users.noreply.github.com>
2018-06-01 16:29:17 +0200

    Update serial_interface.py

--- serial_interface.py
+++ serial_interface.py
@@ -1,22 +1,45 @@
 import serial
+import sys
 
-port = '/dev/ttyUSB2'
+ports = ['/dev/ttyUSB', '/dev/ttyACM', '\\.\COM', '/dev/tty.usbmodem1234']
 baud = 115200
+ser = None
 
-ser = serial.Serial(port, baud, timeout=1)
-
-if ser.isOpen():
-	print(ser.name+' is open ...')
+def open_port():
+	global ser
+	for port in ports:
+		if ser:
+			break
+		for i in xrange(0, 64):
+			try:
+				target_port = port + str(i)
+				ser = serial.Serial(target_port, baud, timeout=1)
+				print(ser.name + ' is open ...' + '\r')
+				break
+				
+			except:
+				pass
+	
 	
 while True:
-	cmd = raw_input("Enter command or 'exit': ")
-	if cmd == 'exit':
-		ser.close()
-		exit()
-	else:
-		ser.write(cmd.encode('ascii')+'\r')
-		while True:
-			out = ser.readline()
-			print('>>'+out)
-			if not out:
-				break
+	try:
+		open_port()
+		cmd = raw_input("Enter command or 'exit': ")
+		if cmd == 'exit':
+			ser.close()
+			exit()
+		else:
+			ser.write(cmd.encode('ascii')+'\r')
+			while True:
+				out = ser.readline()
+				print('>>'+out)
+				if not out:
+					break
+	
+	except serial.SerialException:
+		print ("Monitor: Disconnected (Serial exception)")
+	except IOError:
+		print ("Monitor: Disconnected (I/O Error)")
+	except KeyboardInterrupt:
+		print ("Monitor: Keyboard Interrupt. Exiting Now...")
+		sys.exit(1)	
